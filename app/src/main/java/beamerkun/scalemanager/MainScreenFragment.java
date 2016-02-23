7a568ad3bbbee5c85896c4b2f3d38d3940198401
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2016-02-23 04:18:22 +0100

    Reseach Step 2 - Connect as BlueTooth low energy device and analyze services
    Output simply produced on System.out

--- app/src/main/java/beamerkun/scalemanager/MainScreenFragment.java
+++ app/src/main/java/beamerkun/scalemanager/MainScreenFragment.java
@@ -2,31 +2,28 @@ package beamerkun.scalemanager;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Parcelable;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 /**
  * Mess of a fragment, work heavily in progress
  */
 public class MainScreenFragment extends Fragment {
-
-    public MainScreenFragment() {
-        m_btAdapter = BluetoothAdapter.getDefaultAdapter();
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -46,23 +43,17 @@ public class MainScreenFragment extends Fragment {
         bt_scan_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_arrayAdapter.clear();
-                m_btAdapter.startDiscovery();
+                if(m_btScaleDevice == null)
+                    btLeScan();
+                else
+                    m_btGatt.discoverServices();
             }
         });
         bt_scan_button.setEnabled(m_btAdapter.isEnabled());
 
         IntentFilter filterFound = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         IntentFilter filterStateChanged = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-        getActivity().registerReceiver(m_btListReceiver, filterFound);
         getActivity().registerReceiver(m_btStateReceiver, filterStateChanged);
-
-        m_arrayAdapter = new ArrayAdapter<String>(v.getContext(),
-                android.R.layout.simple_list_item_1,
-                new ArrayList<String>());
-
-        ListView lv = (ListView) v.findViewById(R.id.listView);
-        lv.setAdapter(m_arrayAdapter);
 
         return v;
     }
@@ -70,26 +61,81 @@ public class MainScreenFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        getActivity().unregisterReceiver(m_btListReceiver);
         getActivity().unregisterReceiver(m_btStateReceiver);
     }
 
-    static final private String c_btScaleDeviceName = "VScale";
+    private void btLeScan() {
+        m_btHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                m_btAdapter.stopLeScan(m_btLeScanCallback);
+            }
+        }, c_scanPeriod);
+        m_btAdapter.startLeScan(m_btLeScanCallback);
+    }
 
-    private BluetoothAdapter m_btAdapter;
-    private ArrayAdapter<String> m_arrayAdapter;
+    private void onScaleFound(BluetoothDevice scale) {
+        m_btScaleDevice = scale;
+
+        m_btGatt = m_btScaleDevice.connectGatt(getActivity().getApplicationContext(), true, m_btGattCallback);
+    }
+
+    private static final long c_scanPeriod = 10000;
+    private static final String c_scaleDeviceName = "VScale";
+
+    private Handler m_btHandler = new Handler();
+
+    private BluetoothAdapter m_btAdapter = BluetoothAdapter.getDefaultAdapter();
     private BluetoothDevice m_btScaleDevice = null;
+    private BluetoothGatt m_btGatt = null;
 
-    private final BroadcastReceiver m_btListReceiver = new BroadcastReceiver() {
+    private BluetoothGattCallback m_btGattCallback = new BluetoothGattCallback() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                m_arrayAdapter.add(device.getName() + " , " + device.getAddress());
-                if (device.getName().equals(c_btScaleDeviceName)) {
-                    m_btScaleDevice = device;
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            String intentAction;
+            if(newState == BluetoothProfile.STATE_CONNECTED) {
+                System.out.println("Gatt connected");
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                System.out.println("Gatt disconnected");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if(status == BluetoothGatt.GATT_SUCCESS) {
+                for(BluetoothGattService service : gatt.getServices()) {
+                    if(service.getType() == BluetoothGattService.SERVICE_TYPE_SECONDARY)
+                        continue;
+                    // filter out standard services
+                    if(service.getUuid().toString().endsWith("00805f9b34fb"))
+                        continue;
+                    System.out.printf("service: %s%n", service.getUuid());
+                    for(BluetoothGattCharacteristic characteristic : service.getCharacteristics()) {
+                        System.out.printf("  char perm: %d%n", characteristic.getPermissions());
+                        System.out.printf("  char prop: %d%n", characteristic.getProperties());
+                    }
                 }
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            if(status == BluetoothGatt.GATT_SUCCESS) {
+                System.out.println(characteristic.toString());
+            }
+        }
+    };
+
+    private BluetoothAdapter.LeScanCallback m_btLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            if(c_scaleDeviceName.equals(device.getName())) {
+                // Remove pending scan stop and run it manually
+                m_btHandler.removeCallbacksAndMessages(null);
+                m_btAdapter.stopLeScan(this);
+
+                onScaleFound(device);
             }
         }
     };
